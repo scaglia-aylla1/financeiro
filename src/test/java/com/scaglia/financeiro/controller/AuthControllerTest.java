490a527e21f91c3703cf159d9c56f90b678bdba2
Aylla Scaglia <aylla@scaglia.com.br>
2026-03-21 11:23:17 -0300

    fix(test): desativa filtros de segurança no WebMvcTest

--- src/test/java/com/scaglia/financeiro/controller/AuthControllerTest.java
+++ src/test/java/com/scaglia/financeiro/controller/AuthControllerTest.java
@@ -14,6 +14,7 @@ import com.scaglia.financeiro.service.AuthService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -23,6 +24,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test") // Configura o MockMvc para simular chamadas HTTP
 class AuthControllerTest {
 
