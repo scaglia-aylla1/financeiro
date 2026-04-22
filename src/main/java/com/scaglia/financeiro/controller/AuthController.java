02e09b4b3cca27f4c797284c2ba69a84457e3382
Aylla Scaglia <aylla@scaglia.com.br>
2026-04-22 13:51:43 -0300

    refactor: allow cors for local development environment

--- src/main/java/com/scaglia/financeiro/controller/AuthController.java
+++ src/main/java/com/scaglia/financeiro/controller/AuthController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @Validated
 public class AuthController {
